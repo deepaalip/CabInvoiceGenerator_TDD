59698be3c22589bdfdcd8692e6a1c453bf4f8b5c
deepali <deepaalipatil@gmail.com>
2022-01-18 15:42:44 +0530

    ADDED: Calculation of fare

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+
+public class InvoiceGenerator {
+		private static final int COST_PER_TIME = 1;
+		private static final int MINIMUMCOST_PER_KILOMETER = 10;
+		private static final int MINIMUM_FARE = 5;
+		public double calculateFare(double distance, int time) {
+		
+			double totalFare =  distance * MINIMUMCOST_PER_KILOMETER + time * COST_PER_TIME;
+			if(totalFare < MINIMUM_FARE) {
+				return MINIMUM_FARE;
+			}
+			return totalFare;
+		}
+	
+}
