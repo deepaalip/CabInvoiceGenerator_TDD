1d7b4e2d5643390b1aca3d4b6d685ed1f532df1c
deepali <deepaalipatil@gmail.com>
2022-01-18 21:29:59 +0530

    ADDED: Enhanced invoice

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -5,6 +5,7 @@ public class InvoiceGenerator {
 		private static final int COST_PER_TIME = 1;
 		private static final int MINIMUMCOST_PER_KILOMETER = 10;
 		private static final int MINIMUM_FARE = 5;
+		
 		public double calculateFare(double distance, int time) {
 		
 			double totalFare =  distance * MINIMUMCOST_PER_KILOMETER + time * COST_PER_TIME;
@@ -18,9 +19,17 @@ public class InvoiceGenerator {
 			
 			double totalFare =0;
 			 for(Rides ride : rides) {
-				 totalFare += this.calculateFare(ride.Distance, ride.Time);
+				 totalFare += this.calculateFare(ride.distance, ride.time);
 			 }
 			return totalFare;
 		}
 	
+		public InvoiceSummary calculateFareReturnObject(Rides[] rides) {
+			
+			double totalFare = 0;
+			for(Rides ride : rides) {
+				totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
+			}
+			return new InvoiceSummary(rides.length, totalFare);
+		}
 }
