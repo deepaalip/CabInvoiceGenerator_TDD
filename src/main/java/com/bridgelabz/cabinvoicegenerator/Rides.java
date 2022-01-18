7f335131f430ea296fe474f7f51898b9e03bba75
deepali <deepaalipatil@gmail.com>
2022-01-18 16:16:57 +0530

    ADDED: total fare calculation for multiple rides

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Rides {
+	
+	double Distance;
+	int Time;
+
+	public Rides( double distance,int time) {
+		super();
+		
+		this.Distance = distance;
+		this.Time = time;
+	}
+
+	}
